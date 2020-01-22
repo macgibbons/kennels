582a41074f181c2e406eb96633cf5edcbaf27fc5
macgibbons <macgibbons@gmail.com>
2020-01-22 14:55:20 -0600

    learning the basics of React. modularization and syntax.

---
+++ src/components/customer/Customer.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default () => (
+    <section className="customer">
+        <h3 className="customer__name">Hannah Hall</h3>
+        <address>
+            <div className="customer__address">100 infinity way</div>
+        </address>
+    </section>
+)
---
+++ src/components/location/Location.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default () => (
+    <section className="location">
+        <h3 className="location__name">Nashville Kennels North</h3>
+        <address>
+            <div className="location__address">200 Main Street</div>
+        </address>
+    </section>
+)
