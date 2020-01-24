36acd7c7bfd701dc315b52c6d7ec334a3177ee9e
macgibbons <macgibbons@gmail.com>
2020-01-24 09:55:57 -0600

    now displaying the owner and location of the dog via importing multiple contexts

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,17 +1,28 @@
 import React, { useContext } from "react"
+import { AnimalContext } from "./AnimalProvider"
+import { LocationContext } from "../location/LocationProvider"
+import { CustomerContext } from "../customer/CustomerProvider"
+import Animal from "./Animal"
 import "./Animals.css"
-import { AnimalContext } from "./AnimalProvider";
-import Animal from "./Animal";
-
 
 export default () => {
     const { animals } = useContext(AnimalContext)
+    const { locations } = useContext(LocationContext)
+    const { customers } = useContext(CustomerContext)
 
     return (
         <div className="animals">
-        {
-            animals.map(anml => <Animal key={anml.id} animal={anml} />)
-        }
+            {
+                animals.map(animal => {
+                    const owner = customers.find(c => c.id === animal.customerId)
+                    const clinic = locations.find(l => l.id === animal.locationId)
+
+                    return <Animal key={animal.id}
+                                location={clinic}
+                                customer={owner}
+                                animal={animal} />
+                })                
+            }
         </div>
     )
 }
