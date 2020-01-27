41da273ea7c06b02815e1cf626118fdff59414a3
macgibbons <macgibbons@gmail.com>
2020-01-27 15:35:35 -0600

    more styling

--- src/components/animal/Animal.js
+++ src/components/animal/Animal.js
@@ -1,7 +1,11 @@
 import React from "react"
+import "./dog.svg"
 
 export default ({animal, customer, location}) => (
     <section className="animal">
+        <div>
+        <img className="icon" src={require ('./dog.svg')}/>
+        </div>
         <h3 className="animal__name">{animal.name}</h3>
         <div className="animal__breed">{animal.breed}</div>
         <div className="animal__location">Location: { location.name }</div>
