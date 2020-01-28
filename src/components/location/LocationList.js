bf6c9e654149bc812eaa984ec745d032e8fc3830
macgibbons <macgibbons@gmail.com>
2020-01-28 10:47:18 -0600

    added a header for the location page

--- src/components/location/LocationList.js
+++ src/components/location/LocationList.js
@@ -8,9 +8,18 @@ export default () => {
 
     return (
         <div className="locations">
-        {
-            locations.map(loc => <Location key={loc.id} location={loc} />)
-        }
+            <h1>Nashville Kennels</h1>
+            <div>    
+                <small>Loving care when you're not there.</small>
+                <address>
+                    <div>Visit Us at one of our two Nashville Locations</div>
+                </address>
+            </div>
+            <div className="location__list">
+                {
+                    locations.map(loc => <Location key={loc.id} location={loc} />)
+                }
+            </div>
         </div>
     )
 }
